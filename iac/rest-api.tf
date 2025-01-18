c24475ad062cc589986f7282f62b4e068239b51e
Andre Lopes <andrevitorlopes@gmail.com>
2025-01-18 23:00:12 +0100

    Ad dintegration and method to redpeloyment

--- iac/rest-api.tf
+++ iac/rest-api.tf
@@ -6,8 +6,14 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   triggers = {
     redeployment = sha1(jsonencode([
-      aws_api_gateway_rest_api.rest_api.root_resource_id
+      aws_api_gateway_rest_api.rest_api.root_resource_id,
+      aws_api_gateway_method.method.id,
+      aws_api_gateway_integration.integration.id
     ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
