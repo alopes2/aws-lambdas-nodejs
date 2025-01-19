7d100ee3339d9b558a120996d14cd2de4897ed42
Andre Lopes <andrevitorlopes@gmail.com>
2025-01-19 17:58:53 +0100

    Add throttling for all

--- iac/rest-api.tf
+++ iac/rest-api.tf
@@ -30,6 +30,17 @@ resource "aws_api_gateway_method" "method" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
 }
 
+resource "aws_api_gateway_method_settings" "all_method_settings" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = aws_api_gateway_stage.stage.stage_name
+  method_path = "/*/*"
+
+  settings {
+    throttling_burst_limit = 1
+    throttling_rate_limit  = 1
+  }
+}
+
 resource "aws_api_gateway_method_settings" "method_settings" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   stage_name  = aws_api_gateway_stage.stage.stage_name
