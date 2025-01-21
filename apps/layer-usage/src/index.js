57d055b4f996c001f6bdf258d4a17d81f8ae5ff7
Andre <andrevitorlopes@gmail.com>
2025-01-21 20:20:25 +0100

    Fix handler

--- apps/layer-usage/src/index.js
+++ apps/layer-usage/src/index.js
@@ -1,11 +1,9 @@
 import myModule from './dependencies.js';
 import _ from 'lodash';
 
-const handler = (event) => {
+export const handler = (event) => {
   return {
     lodash: _.random(true) * 100,
     myModule: myModule.capitalize(event.name),
   };
 };
-
-console.log(handler({ name: 'yay' }));
