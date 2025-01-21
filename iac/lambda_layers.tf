84c39b0ff374ab25c5ebcdb1aade6eeab1fb260c
Andre <andrevitorlopes@gmail.com>
2025-01-21 20:16:56 +0100

    Try source_code_hash with last modified

--- iac/lambda_layers.tf
+++ iac/lambda_layers.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_layer_version" "layer" {
   layer_name          = "nodejs-meetup-layer"
   s3_bucket           = aws_s3_bucket.layers.bucket
   s3_key              = data.aws_s3_object.layer.key
-  source_code_hash    = filebase64sha256(data.aws_s3_object.layer.key)
+  source_code_hash    = base64sha256(data.aws_s3_object.layer.last_modified)
   # s3_object_version = aws_s3_bucket_key  // This is a better approach then the source_code_hash, but requires your s3_bucket to have version enabled
   # filename          = "layers/layer.zip" // This you can make use of the "archive_file" data source, or zip for yourself
 }
