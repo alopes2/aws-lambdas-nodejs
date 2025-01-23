545873861363591b0dc42baa65bcb80ce03aac0a
Andre Lopes <andrevitorlopes@gmail.com>
2025-01-23 20:54:15 +0100

    Remove s3 key reference

--- iac/lambda_layers.tf
+++ iac/lambda_layers.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_layer_version" "layer" {
   compatible_runtimes = ["nodejs22.x"]
   layer_name          = "nodejs-meetup-layer"
   s3_bucket           = aws_s3_bucket.layers.bucket
-  s3_key              = data.aws_s3_object.layer.key
+  # s3_key              = data.aws_s3_object.layer.key
   # source_code_hash    = base64sha256(data.aws_s3_object.layer.last_modified)
   # s3_object_version = aws_s3_bucket_key  // This is a better approach then the source_code_hash, but requires your s3_bucket to have version enabled
   # filename          = "layers/layer.zip" // This you can make use of the "archive_file" data source, or zip for yourself
