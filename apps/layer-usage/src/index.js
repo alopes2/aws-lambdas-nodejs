89c5cd9624b8b01e961847e67ab683ec8b0a5824
Andre Lopes <andrevitorlopes@gmail.com>
2025-01-23 20:09:00 +0100

    Fix layer-usage lambda

--- apps/layer-usage/src/index.js
+++ apps/layer-usage/src/index.js
@@ -1,7 +1,7 @@
 import myModule from './dependencies.js';
 import _ from 'lodash';
 
-export const handler = (event) => {
+export const handler = async (event) => {
   return {
     lodash: _.random(true) * 100,
     myModule: myModule.capitalize(event.name),
